751c5303c5ede3bbb23168cc9fe24794d98b9de9
paulognu <85591305+paulognu@users.noreply.github.com>
2023-12-16 15:20:53 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,10 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 AS SOMA;");
+  console.log(result.rows);
   response.status(200).json({
-    status: "são acima da média",
+    chave: "são acima da média",
   });
 }
 
